b9e742f4f5733fd336b09572d0b583ea6acbc957
George C <george.constantin33@gmail.com>
2024-05-08 13:56:31 +0100

    fixed bugs on game start

--- start.js
+++ start.js
@@ -1,4 +1,11 @@
 function startGame(SNAKE_SPEED) {
+  document.removeEventListener("keydown", handleMenuElementKeyDown);
+  document.addEventListener("keydown", handleKeyDown);
+  currentScore = 0;
+  resetBoard();
+  updateScoreView();
+  SNAKE_ARRAY.length = 0;
+  DIRECTION = DIRECTIONS.LEFT;
   startMenu.style.visibility = "hidden";
   createSnake(5, Math.floor(BOARD_WIDTH / 2), Math.floor(BOARD_HEIGHT / 2));
   generateFood();
